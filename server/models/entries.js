2874a218bdb9090e566c4af9937a90431a451dce
Delicelydia <Delydia84@gmail.com>
2019-09-11 17:06:56 +0200

    ft(specificEntry): add specificEntry
    created file in the controllers
    for specific entry
    create endpoint for specific entry
    
    [Delivers #168343895]

--- server/models/entries.js
+++ server/models/entries.js
@@ -1,11 +1,13 @@
 const entries = [];
 
 const entry1 ={
+    entryId: 1,
     createdOn : '10/09/2019',
     title: 'morning routine',
     entry: 'when you wake up in the morning remember to thank God',
 };
 const entry2 ={
+    entryId: 2,
     createdOn : '11/09/2019',
     title: 'weekend',
     entry: 'after a long week working hard weekend i relax',
