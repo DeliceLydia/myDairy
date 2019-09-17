efce2bd4b18d9717a79c5e97a0669480ab725059
Lydia Delice <Lydia@ASL-MacBook-Pro.local>
2019-09-17 12:16:16 +0200

    add .env file

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
     });
   });
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT, () => {
     console.log(`server is running on PORT ${PORT}`);
  });
 export default app;
