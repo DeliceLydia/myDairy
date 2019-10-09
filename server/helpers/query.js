b46cae19172db815585d2f61a695a70469663c88
lydia delice <delydia84@gmail.com>
2019-10-09 21:32:31 +0200

    ft(modify): add test fix endpoints
    [#169043273]

--- server/helpers/query.js
+++ server/helpers/query.js
@@ -3,16 +3,18 @@ const sql = {};
 const addUser = `INSERT INTO users(firstName, lastName, email, password) 
 VALUES ( $1, $2, $3, $4) Returning*`; 
 const findUser =  `SELECT * FROM users WHERE email = $1`;
-
-
-
-
-
-
+const postEntry = `INSERT INTO entries (date, title, entry)
+ VALUES ($1, $2, $3) RETURNING *`;
+const getAll = `SELECT * FROM entries`;
+const findEntrybyId = `SELECT * FROM entries WHERE id = $1`;
+const updateEntry = `UPDATE entries SET `
 
 
 
 sql.addUser = addUser;
 sql.findUser = findUser;
+sql.postEntry = postEntry;
+sql.getAll = getAll;
+sql.findEntrybyId = findEntrybyId;
 
 export default sql;
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -12,7 +12,7 @@ const auth = (req, res, next) => {
         });
     }
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
         req.user = decoded;
         next();
     } catch (ex) {
